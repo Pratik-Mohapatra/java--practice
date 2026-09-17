2a9314899e2a9ef966d0cedcef13281c3e49b39b
pratik mohapatra <mohapatrapratik75@gmail.com>
2026-09-17 18:42:18 +0530

    for loop basics

---
+++ For_loops.java
@@ -0,0 +1,32 @@
+public class For_loops {
+    public static void main(String[] args) {
+        //for loops 
+         for (int i=0;i<10;i++)
+        {
+            System.out.println("hello bro!!!");
+
+        }
+        
+
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("hello bro!!!");
+
+        }
+            
+
+        for(int i=0;i<10;i++)
+        {
+            System.out.println(i);
+
+        }
+            
+
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+    
+}
